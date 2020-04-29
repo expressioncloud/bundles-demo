ba1a5dbdeeaa9525b7465ec3183dfe8dd840c7c9
dzz <devon.zachary@gmail.com>
2020-04-29 14:07:19 -0700

    update demo

--- xpr/server_js/ajax/script.js
+++ xpr/server_js/ajax/script.js
@@ -1,5 +1,11 @@
 exports.process = function(context, options) {
+
+    let message = "Hello World";
+    let user_message = require("/xpr/request/").XprRequest.urlParams["message"]
+
+    message = user_message ? user_message : message;
+
     return { 
-        "message" : "Hello World"
+        "message" : user_message
     };
 }
